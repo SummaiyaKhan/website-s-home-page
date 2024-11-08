fc3ed2d9f74ea19131d82b02cfb61befad0edf92
SummaiyaKhan98 <nk4937169@gmail.com SummaiyaKhan98>
2024-11-08 12:22:42 -0800

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,11 +8,11 @@ import { FaStar } from "react-icons/fa"; // Star icon for top choice
 
 export default function Home() {
   return (
-    <div className="w-[1366px]">
-      
+    <div className="max-w-screen-xl mx-auto">
+
       {/* Header and Navigation */}
       <div className="bg-[#890620] relative">
-        <nav className="w-[1300px] py-5 flex justify-between text-[#EBD4CB] cursor-pointer">
+        <nav className="w-full py-5 flex justify-between text-[#EBD4CB] cursor-pointer">
           <h1 className="ml-20 text-xl font-serif hover:text-white">MYSTARA</h1>
           <ul className="flex justify-end items-end space-x-20">
             <li className="hover:text-white">Home</li>
@@ -30,12 +30,15 @@ export default function Home() {
         {/* Main Content Section */}
         <div className="flex ml-12 mt-5">
           {/* Left Side Image */}
-          <Image  
-            src={image1} 
-            alt="Image" 
-            width={600} 
-            height={600} 
-          />
+          <div className="flex-shrink-0">
+            <Image  
+              src={image1} 
+              alt="Image"
+              layout="intrinsic"
+              width={600} 
+              height={600} 
+            />
+          </div>
 
           {/* Right Side Content */}
           <div className="w-[450px] ml-20 py-[70px]">
@@ -43,7 +46,7 @@ export default function Home() {
               Enchant Your Senses With Affordable Luxury
             </h2>
             <p className="mb-6 text-[#EBD4CB] text-base leading-7">
-              Step into a world of elegance and luxury with our exquisite collection of perfumes. Each fragrance is meticulously crafted to evoke emotion, and leave an unforgettable impression.
+              Step into a world of elegance and luxury with our exquisite collection of perfumes Each fragrance is meticulously crafted to evoke emotion and leave an unforgettable impression
             </p>
             {/* Collection and Follow Buttons */}
             <Button className="bg-black hover:bg-[#1A090D] text-white outline outline-2 outline-black px-6 py-2 rounded-md mb-3 transition-transform duration-300 ease-in-out transform hover:scale-105 hover:shadow-lg">
@@ -57,8 +60,8 @@ export default function Home() {
       </div>
 
       {/* Awards Section */}
-      <div>
-        <div className="p-4 w-[700px] h-48 bg-[#EBD4CB] rounded-br-[50px] rounded-tl-[50px] flex ml-[550px] absolute top-[449px]">
+      <div className="relative mt-20">
+        <div className="p-4 w-[700px] h-48 bg-[#EBD4CB] rounded-br-[50px] rounded-tl-[50px] flex ml-[550px]">
           
           {/* First Award Section */}
           <div className="mt-[33px]">
@@ -66,7 +69,7 @@ export default function Home() {
           </div>
           <div className="w-[250px] flex flex-col justify-center ml-4">
             <h3 className="text-2xl font-medium text-[#890620]">Prestigious Award</h3>
-            <p>Recipient of the 2024 International Perfumer's Guild Award</p>
+            <p>Recipient of the 2024 International Perfumers Guild Award</p>
           </div>
 
           {/* Divider Line */}
@@ -80,7 +83,7 @@ export default function Home() {
           </div>
           <div className="w-[250px] flex flex-col justify-center ml-4">
             <h3 className="text-2xl font-medium text-[#890620]">Top Choice</h3>
-            <p>Featuring "Elixir of Enchantment", our top-selling fragrance for 3 consecutive years</p>
+            <p>Featuring <blockquote><q>Elixir of Enchantment</q></blockquote> our top selling fragrance for 3 consecutive years</p>
           </div>
         </div>
       </div>
